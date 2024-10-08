893d7c542c73b71f7d934a7a5e21a62f59ad97a8
edudevjob <eduardotecnologo@hotmail.com.com>
2024-10-08 07:45:44 -0300

    feat: create calc simple

---
+++ Calc/Calc.cpp
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Calc.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TFrmPrincipal *FrmPrincipal;
+//---------------------------------------------------------------------------
+__fastcall TFrmPrincipal::TFrmPrincipal(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
